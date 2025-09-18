9d4f6115c61bfdf3b65b340c8826e06ac60ec9a7
exyzte <davidecicerys@gmail.com>
2025-09-18 16:32:38 -0500

    abundance comes to me

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Posts from './components/Posts';
+import PostList from './components/PostList';
+import Nav from './components/Nav';
 
 import './App.css'
+
 
 function App() {
   
 
   return (
     <>
-    <div>Welcome to the React Redux Crud Application.</div>
+    <div className='App'>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route exact path="/Home" element={ <Home /> }/> {/* path is the name you give to the route, exatc helps the browser understand that it should only return the parent route when there is an exact math on the URL*/}
+          <Route exact path="/About" element={ <About /> } />
+          <Route exact path="/Contact" element={ <Contact />} />
+          <Route exact path="/Posts" element={ <Posts />} >
+            <Route path="postList" element={ <PostList />} />
+          </Route>
+          <Route exact path="/PostList" element={ <PostList />} />
+        </Routes>
+      </Router>
+      
+      </div>
     </>
   )
 }
---
+++ src/components/PostList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PostList() {
+  return (
+    <div>
+      postlist
+    </div>
+  )
+}
+
+export default PostList
