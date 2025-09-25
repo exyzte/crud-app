f2647fdf69a7f195bac7fb40431c4ccdb7379ad3
exyzte <davidecicerys@gmail.com>
2025-09-25 12:09:44 -0500

    added svg icon for user profile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,11 +5,11 @@ import'../CSS/main.css'
 function Nav() {
   return (
     <header>
-      <Link to="/"><h1 className="logo">CRUD App</h1></Link>
+      <Link to="/"><h1 className="logo">Wayra</h1></Link>
     <nav className="navbar">
       <NavLink to="/posts" className="navlink">posts</NavLink>
       <NavLink to="/about" className="navlink">about</NavLink>
-      <NavLink to="/contact" className="navlink">contact</NavLink>
+      <NavLink to="/contact" className="navlink contact"><img src="" alt="user-icon" />User</NavLink>
     </nav>
     </header>
   )
